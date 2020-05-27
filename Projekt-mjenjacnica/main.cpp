1cb77c59fed7571f70de2662b9a20e1e2aaa4076
Demy2204 <dzlovro@gmail.com>
2020-05-27 15:42:10 +0200

    Update main.cpp
    opcije mijenjanja valuta

--- Projekt-mjenjacnica/main.cpp
+++ Projekt-mjenjacnica/main.cpp
@@ -13,7 +13,7 @@ int main()
     const double CeskaKrunaTecaj = 0.2795;
     const double ForinteTecaj = 2.1675;
 
-    int izbor;
+    int izbor, izborvalute1, svota;
     while(1)
     {
 
@@ -38,6 +38,73 @@ int main()
          else if(izbor == 2)
          {
           //pitat odma za valute
+            cout << "Koju valutu zelite promijeniti?" << endl;
+            while(1)
+                {
+                    cout << "1. Euro" << endl;
+                    cout << "2. Dolar" << endl;
+                    cout << "3. Marka" << endl;
+                    cout << "4. Funta" << endl;
+                    cout << "5. Kruna" << endl;
+                    cout << "6. Forinte" << endl;
+                    cout << "Unesite broj valute: ";
+                    cin >> izborvalute1;
+
+                    if(izborvalute1 == 1)
+                      {
+                           cout << "Odabrali ste euro." << endl;
+                           cout << "Unesite svotu koliko zelite promijeniti." << endl;
+                           cin >> svota;
+                           cout << "U koju valutu zelite promijeniti svoj iznos?" << endl;
+
+                      }
+                    else if(izborvalute1 == 2)
+                      {
+                           cout << "Odabrali ste dolar." << endl;
+                           cout << "Unesite svotu koliko zelite promijeniti." << endl;
+                           cin >> svota;
+                           cout << "U koju valutu zelite promijeniti svoj iznos?" << endl;
+
+                      }
+                     else if(izborvalute1 == 3)
+                      {
+                           cout << "Odabrali ste marku." << endl;
+                           cout << "Unesite svotu koliko zelite promijeniti." << endl;
+                           cin >> svota;
+                           cout << "U koju valutu zelite promijeniti svoj iznos?" << endl;
+
+                      }
+                     else if(izborvalute1 == 4)
+                      {
+                           cout << "Odabrali ste funtu." << endl;
+                           cout << "Unesite svotu koliko zelite promijeniti." << endl;
+                           cin >> svota;
+                           cout << "U koju valutu zelite promijeniti svoj iznos?" << endl;
+
+                      }
+                     else if(izborvalute1 == 5)
+                      {
+                           cout << "Odabrali ste krunu." << endl;
+                           cout << "Unesite svotu koliko zelite promijeniti." << endl;
+                           cin >> svota;
+                           cout << "U koju valutu zelite promijeniti svoj iznos?" << endl;
+
+                      }
+                     else if(izborvalute1 == 6)
+                      {
+                           cout << "Odabrali ste forinte." << endl;
+                           cout << "Unesite svotu koliko zelite promijeniti." << endl;
+                           cin >> svota;
+                           cout << "U koju valutu zelite promijeniti svoj iznos?" << endl;
+
+                      }
+                    else
+                    {
+                        cout << "Krivi unos, unesite broj ponovno" << endl;
+                    }
+
+                }
+
          }
          else if(izbor == 3)
          {
